ccd88aa96a2f578a9948feb83d4b8da9e9807278
Svetoslav Nikolov <135174208+nsvetoslav@users.noreply.github.com>
2024-02-12 03:12:50 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-const channelId = '1187321681405022261';
+const channelId = '';
 
 client.once('ready', async () => {
   console.log(`Logged in as ${client.user?.tag}!`);
@@ -13,4 +13,4 @@ client.once('ready', async () => {
   loadNewTokens(channel);
 });
 
-client.login('MTE4NzMyMjAzNTcxMDQ2NDAwMA.GiEOV9.PDsXKISSL30gF_zeVOtR1bc2Ac903GCq6FNwzs');
+client.login('');
